e893fd05116c16ed08d563df5ac096997b85f59f
Mo McRoberts <mo@nevali.net>
2018-02-22 20:06:48 +0000

    Add simple XML parse/dump routine

--- scene.cc
+++ scene.cc
@@ -1,17 +1,69 @@
 #include <iostream>
 
-#include <libxml/SAX.h>
+#include <libxml/xmlreader.h>
 
 class Scene
 {
 public:
 	int load(const char *pathname);
+protected:
+	int processNode(xmlTextReaderPtr reader);
 };
 
 int
 Scene::load(const char *pathname)
 {
-	return -1;
+	xmlTextReaderPtr reader;
+	int ret;
+
+	reader = xmlReaderForFile(pathname, NULL, 0);
+	if (!reader)
+	{
+		return -1;
+	}
+	ret = xmlTextReaderRead(reader);
+	/* xmlTextReaderRead() returns 1 for 'more remaining', 0 for 'complete',
+	 * any other value for an error
+	 */
+	while(ret == 1)
+	{
+		if((ret = processNode(reader)))
+		{
+			/* processNode() returns non-zero on error */
+			break;
+		}
+		ret = xmlTextReaderRead(reader);
+	}
+	xmlFreeTextReader(reader);
+	if(ret)
+	{
+		/* At this point, ret should be zero if the file was
+		 * processed successfully; as it's non-zero here, we
+		 * know an error occurred.
+		 */
+		return -1;
+	}
+	return 0;
+}
+
+int
+Scene::processNode(xmlTextReaderPtr reader)
+{
+	const xmlChar *name, *ns;
+	int type;
+	
+	type = xmlTextReaderNodeType(reader);
+	ns = xmlTextReaderConstNamespaceUri(reader);
+	name = xmlTextReaderConstName(reader);
+	if(ns)
+	{
+		std::clog << "Found node type " << type << ", name = '" << ((const char *) ns) << ((const char *) name) << "'\n";
+	}
+	else
+	{
+		std::clog << "Found node type " << type << ", name = '" << ((const char *) name) << "'\n";
+	}
+	return 0;
 }
 
 int
@@ -19,6 +71,11 @@ main(int argc, char **argv)
 {
 	Scene s;
 	
+	/* Ensure that we're linked against a compatible version of the
+	 * libxml2 runtime
+	 */
+	LIBXML_TEST_VERSION;
+	/* Check command-line arguments */
 	if(argc != 2)
 	{
 		std::cerr << "Expected a single scene file name argument.\n";
@@ -27,10 +84,15 @@ main(int argc, char **argv)
 			"  " << argv[0] << " FILENAME\n";
 		return 0;
 	}
+	/* Attempt to load the scene from the named file */
 	if(s.load(argv[1]))
 	{
 		std::cerr << argv[0] << ": failed to load scene '" << argv[1] << "'.\n";
 		return 1;
 	}
+	/* We don't DO anything with the loaded scene, so we can just
+	 * return success after cleaning up.
+	 */
+	xmlCleanupParser();
 	return 0;
 }
