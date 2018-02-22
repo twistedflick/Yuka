df211b9debc73179cddcc109b9a30c1781eb4baa
Mo McRoberts <mo@nevali.net>
2018-02-22 01:41:16 +0000

    Add argument count check and usage message

--- scene.cc
+++ scene.cc
@@ -19,6 +19,14 @@ main(int argc, char **argv)
 {
 	Scene s;
 	
+	if(argc != 2)
+	{
+		std::cerr << "Expected a single scene file name argument.\n";
+		
+		std::cout << "\nUsage:\n\n" <<
+			"  " << argv[0] << " FILENAME\n";
+		return 0;
+	}
 	if(s.load(argv[1]))
 	{
 		std::cerr << argv[0] << ": failed to load scene '" << argv[1] << "'.\n";
